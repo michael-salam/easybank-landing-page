9139dfe75f71dc509309cbe263b017657406d90d
michael-salam <84015713+michael-salam@users.noreply.github.com>
2022-04-05 22:00:40 +0100

    Completed the mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const btnHamburger = document.querySelector("#btnHamburger");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", function () {
+  if (header.classList.contains("open")) {
+    // Close Hamburger Menu
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    // Open Hamburger Menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
